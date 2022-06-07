83248888b5eeef1140808187bbca33e376eaa599
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-07 15:23:06 -0700

    create Stack class. passing test

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(initialValue) {
+    if (initialValue) {
+      this.#list = initialValue;
+    }
+  }
+
+  push(item) {
+    this.#list.push(item);
+  }
+
+  pop(item) {
+    this.#list.pop(item);
+  }
+
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+}
 
 class Queue {}
 
--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
-const { Stack } = require("./index.js");
-describe("Stack Class", () => {
-  it("#push should add a new item to the stack ", () => {
+const { Stack } = require('./index.js');
+
+describe('Stack Class', () => {
+  it('#push should add a new item to the stack ', () => {
     const stack = new Stack();
     stack.push(1);
     expect(stack.peek()).toEqual(1);
